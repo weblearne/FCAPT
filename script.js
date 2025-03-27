39ecc4692ea9e67bd48be26bf1ce9c6286fe7cfc
Basiru Lawan <79064498+weblearne@users.noreply.github.com>
2025-03-27 22:51:42 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -2,15 +2,12 @@ function login(event){
     let gmail = document.getElementById("gmail").value;
     let password = document.getElementById("password").value;
     event.preventDefault();
-    
+
     if(gmail === "user@gmail.com" && password === "user123"){
-        window.location.href="user_dashboard.html"
+      window.location.href="user_dashboard.html"
     }else if(gmail === "admin@gmail.com" && password === "admin123"){
-        window.location.href="admin.html"
+        window.location.href="admindashboard.html"
     }else{
-        window.location.href="index.html"
-}
-    
-
-
+        alert('invalid gmail or password');
+    }
 }
